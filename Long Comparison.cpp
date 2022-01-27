5e9626b9e742b7e10bef824bbfd146f0c96183ce
aquila-shivam <72098611+aquila-shivam@users.noreply.github.com>
2022-01-27 08:08:30 +0530

    Update Long Comparison.cpp

--- Long Comparison.cpp
+++ Long Comparison.cpp
@@ -1,3 +1,18 @@
+
+// Long Comparison
+
+
+// Input
+// The first line contains a single integer t (1≤t≤104) — the number of testcases.
+
+// The first line of each testcase contains two integers x1 and p1 (1≤x1≤106;0≤p1≤106) — the description of the first number.
+
+// The second line of each testcase contains two integers x2 and p2 (1≤x2≤106;0≤p2≤106) — the description of the second number.
+
+// Output
+// For each testcase print the result of the comparison of the given two numbers. If the first number is smaller than the second one, print '<'. If the first number is greater than the second one, print '>'. If they are equal, print '='.
+
+
 #include <bits/stdc++.h>
  
 using namespace std;
